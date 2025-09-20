de474c0a4a83f0615269b2ecbc4b9ecfd88d62f9
davi <davicastro213@gmail.com>
2025-09-20 17:54:51 -0300

    feat:adicione icones e modal do formulario

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -5,9 +5,9 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-     <div className="flex min-h-svh w-full bg-[url('/fachada-Copy.jpeg')] bg-cover bg-center bg-no-repeat items-center justify-center p-6 md:p-10">
+     <div className="flex min-h-svh w-full bg-[url('/fachada-Copy.jpeg')]  backdrop-blur-lg bg-cover bg-center bg-no-repeat items-center justify-center p-6 md:p-10">
     <Toaster position="top-center" richColors />
-     <div className="w-full max-w-sm">
+     <div className="w-full max-w-sm ">
         {children}
       </div>
     </div>
--- src/components/form/input/TextArea.tsx
+++ src/components/form/input/TextArea.tsx
@@ -27,7 +27,7 @@ const TextArea: React.FC<TextareaProps> = ({
     }
   };
 
-  let textareaClasses = `w-full rounded-lg border px-4 py-2.5 text-sm shadow-theme-xs focus:outline-hidden ${className}`;
+  let textareaClasses = `w-full text-black rounded-lg border px-4 py-2.5 text-sm shadow-theme-xs focus:outline-hidden ${className}`;
 
   if (disabled) {
     textareaClasses += ` bg-gray-100 opacity-50 text-gray-500 border-gray-300 cursor-not-allowed dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700`;
@@ -50,7 +50,7 @@ const TextArea: React.FC<TextareaProps> = ({
       {hint && (
         <p
           className={`mt-2 text-sm ${
-            error ? "text-error-500" : "text-gray-500 dark:text-gray-400"
+            error ? "text-error-500" : "text-black "
           }`}
         >
           {hint}
