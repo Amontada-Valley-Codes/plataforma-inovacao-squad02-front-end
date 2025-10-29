058e147dbe09506f67e17f88d9dd3ff84f7f2a75
davi <davicastro213@gmail.com>
2025-10-29 19:38:13 -0300

    resolver erro deploy

--- src/services/axiosServices.ts
+++ src/services/axiosServices.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 10000,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`, 
+  timeout: 10000, 
   headers: {
-    "Content-Type": "application/json",
-  },
+    "Content-Type": "application/json" 
+  }
 });
 
+
 api.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
-    if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  const token = localStorage.getItem("token");
+  // console.log(token)
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
